02b52e8b32d42af21efd657368e00474223513e4
buxogff <buxogff@gmail.com>
2026-07-06 18:39:53 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./lib/storagePolyfill.js";
 import "./index.css";
 import App from "./App.jsx";
+import AuthGate from "./AuthGate.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthGate>
+      <App />
+    </AuthGate>
   </React.StrictMode>
 );
 
